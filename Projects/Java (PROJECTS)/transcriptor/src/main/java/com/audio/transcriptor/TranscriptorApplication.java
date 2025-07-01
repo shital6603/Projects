2be005f4c32eae6773986be7ffcedc67854c7b04
Shital Khairnar <shital6603@gmail.com>
2025-07-01 21:21:20 +0530

    New springboot project developed

---
+++ Projects/Java (PROJECTS)/transcriptor/src/main/java/com/audio/transcriptor/TranscriptorApplication.java
@@ -0,0 +1,13 @@
+package com.audio.transcriptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranscriptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranscriptorApplication.class, args);
+	}
+
+}
---
+++ Projects/Java (PROJECTS)/transcriptor/src/test/java/com/audio/transcriptor/TranscriptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audio.transcriptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranscriptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
