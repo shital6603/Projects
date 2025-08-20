8a2cb3d7082fa58a258373cec8a8d0cbedad88b6
Shital Khairnar <shital6603@gmail.com>
2025-08-20 16:35:45 +0530

    Changes  to vite.confige file

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Shital-Projects/',   // 👈 add this (must match your repo name exactly!)
+  base: '/Shital-Projects/',   // 👈 add this line
   server: {
     port: 3000,
   },
