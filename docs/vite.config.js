278466d99b5251c46b5e3a63fdfc6bf4e1b5ab22
Shital Khairnar <shital6603@gmail.com>
2025-08-20 16:26:37 +0530

    Files are edited

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Shital-Projects/',   // 👈 add this (must match your repo name exactly!)
   server: {
     port: 3000,
   },
