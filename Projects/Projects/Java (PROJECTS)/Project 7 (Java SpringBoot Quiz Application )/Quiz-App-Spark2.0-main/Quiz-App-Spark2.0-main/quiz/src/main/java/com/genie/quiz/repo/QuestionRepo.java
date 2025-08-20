79f5126fd07a72b53b1a66e67b1499e2dceab155
Shital Khairnar <shital6603@gmail.com>
2025-08-20 15:36:56 +0530

    minor changes of directory

--- Projects/Projects/Java (PROJECTS)/Project 7 (Java SpringBoot Quiz Application )/Quiz-App-Spark2.0-main/Quiz-App-Spark2.0-main/quiz/src/main/java/com/genie/quiz/repo/QuestionRepo.java
+++
@@ -1,9 +0,0 @@
-package com.genie.quiz.repo;
-
-import com.genie.quiz.entity.QuizQuestion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionRepo extends JpaRepository<QuizQuestion,Long> {
-}
